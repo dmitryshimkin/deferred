c45bf7a1dc9222c068a4966a0c4059f1db165c22
Dmitry Shimkin <d.shimkin@corp.mail.ru>
2013-11-25 20:11:01 +0400

    add tests for fail method

--- test/spec/Deferred.spec.js
+++ test/spec/Deferred.spec.js
@@ -14,9 +14,13 @@
       __log = [];
     });
 
+    // exists
+
     it('should exists', function () {
       expect(window.Deferred).toBeDefined();
     });
+
+    // done
 
     describe('done', function () {
 
@@ -148,7 +152,135 @@
     });
 
     // fail
-    // describe
+
+    describe('fail', function () {
+
+      it('handler', function () {
+        var d = new Deferred();
+        var spy = jasmine.createSpy('fail');
+        var rejected = false;
+
+        d.fail(spy);
+
+        runs(function () {
+          setTimeout(function () {
+            d.reject();
+            rejected = true;
+          }, 30);
+        });
+
+        waitsFor(function () {
+          return rejected;
+        }, 'timeout', 100);
+
+        runs(function () {
+          expect(spy).toHaveBeenCalled();
+        });
+      });
+
+      it ('handler with context', function () {
+        var d = new Deferred();
+        var rejected = false;
+        var ctx;
+        var obj = {
+          fn: function () {
+            ctx = this;
+          }
+        };
+
+        d.fail(obj.fn, obj);
+
+        runs(function () {
+          setTimeout(function () {
+            d.reject();
+            rejected = true;
+          }, 30);
+        });
+
+        waitsFor(function () {
+          return rejected;
+        }, 'timeout', 100);
+
+        runs(function () {
+          expect(ctx).toBe(obj);
+        });
+      });
+
+      it('many handlers', function () {
+        var d = new Deferred();
+        var rejected = false;
+
+        var handler1 = function () {
+          __log.push(1);
+        };
+        var handler2 = function () {
+          __log.push(2);
+        };
+        var handler3 = function () {
+          __log.push(3);
+        };
+
+        d
+          .fail(handler1)
+          .fail(handler2)
+          .fail(handler3);
+
+        runs(function () {
+          setTimeout(function () {
+            d.reject();
+            rejected = true;
+          }, 30);
+        });
+
+        waitsFor(function () {
+          return rejected;
+        }, 'timeout', 100);
+
+        runs(function () {
+          expect(__log.length).toBe(3);
+          expect(__log[0]).toBe(1);
+          expect(__log[1]).toBe(2);
+          expect(__log[2]).toBe(3);
+        });
+      });
+
+      it('sync call', function () {
+        var d = new Deferred();
+        var spy = jasmine.createSpy('fail');
+
+        d
+          .fail(spy)
+          .reject();
+
+        expect(spy).toHaveBeenCalled();
+      });
+
+      it('rejected promise', function () {
+        var d = new Deferred();
+        var spy1 = jasmine.createSpy('fail1');
+        var spy2 = jasmine.createSpy('fail2');
+        var spy3 = jasmine.createSpy('fail3');
+
+        d
+          .fail(spy1)
+          .reject()
+          .fail(spy2)
+          .fail(spy3);
+
+        expect(spy1).toHaveBeenCalled();
+        expect(spy1.calls.length).toBe(1);
+
+        expect(spy2).toHaveBeenCalled();
+        expect(spy2.calls.length).toBe(1);
+
+        expect(spy3).toHaveBeenCalled();
+        expect(spy3.calls.length).toBe(1);
+      });
+
+      it('nested', function () {
+
+      });
+    });
 
     // resolve (check double resolve, check resolve of rejected)
 
