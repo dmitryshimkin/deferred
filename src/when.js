81ba32e8c5d00868a61352c2ea7d4b2849add89d
Dmitry Shimkin <d.shimkin@corp.mail.ru>
2014-02-04 22:25:14 +0400

    working on Deferred.when

--- src/when.js
+++ src/when.js
@@ -1,3 +1,38 @@
-Deferred.when = function () {
+/**
+ *
+ * @param promises {Array}
+ * @returns {Promise}
+ */
 
+Deferred.when = function (promises) {
+  var d = new Deferred();
+  var promise, value;
+  var remain = promises.length;
+  var values = [];
+
+  var done = function () {
+    remain = remain - 1;
+    if (remain === 0) {
+      d.resolve();
+    }
+  };
+
+  var fail = function (reason) {
+    d.reject(reason);
+  };
+
+  for (var i = 0, l = promises.length; i < l; i++) {
+    promise = promises[i];
+    promise = promise.promise || promise;
+
+    if (promise._state === REJECTED) {
+      return d.reject(promise.value).promise;
+    }
+
+    promise
+      .done(done)
+      .fail(fail);
+  }
+
+  return d.promise;
 };
--- test/spec/when.spec.js
+++ test/spec/when.spec.js
@@ -7,7 +7,7 @@ describe('Promise.when', function () {
     var spy = jasmine.createSpy('done');
     var obj = { foo: 'bar' };
 
-    var promise = Deferred.when([d1, d2, d3]);
+    var promise = Deferred.when([d1, d2.promise, d3]);
 
     promise.done(spy);
 
@@ -33,7 +33,7 @@ describe('Promise.when', function () {
     var spy = jasmine.createSpy('fail');
     var obj = { foo: 'bar' };
     var e = new TypeError;
-    var promise = Deferred.when([d1, d2, d3]);
+    var promise = Deferred.when([d1.promise, d2, d3]);
 
     promise.fail(spy);
 
@@ -46,24 +46,20 @@ describe('Promise.when', function () {
     d1.reject(e);
 
     expect(promise.isRejected()).toBe(true);
-    expect(spy).toHaveBeenCalledWith([e], [], []);
+    expect(spy).toHaveBeenCalledWith(e);
   });
 
-  it('context', function () {
-    var d1 = new Deferred();
-    var d2 = new Deferred();
-    var d3 = new Deferred();
-    var ctx = { foo: 'bar' };
-    var e = new TypeError;
-    var promise = Deferred.when([d1, d2, d3]);
-    var _this;
+  xit('rejected argument', function () {
 
-    promise.fail(function () {
-      _this = this;
-    });
-
-    d2.reject(e);
-
-    expect(_this).toBe(ctx);
   });
+
+  xit('resolved argument', function () {
+
+  });
+
+  xit('value instead promise', function () {
+
+  });
+
+  // array or simple list
 });
