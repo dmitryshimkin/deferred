693892d7f1a61a85af6975d58621f7ca7a16c8ea
Dmitry Shimkin <dmitryshimkin@gmail.com>
2015-10-30 09:11:05 +0100

    Add karma

--- test/spec/Deferred.resolve.spec.js
+++ test/spec/Deferred.resolve.spec.js
@@ -1,47 +1,53 @@
 describe('Deferred.resolve', function () {
   'use strict';
 
-  it('Exists', function () {
-    expect(typeof Deferred.resolve).toBe('function');
+  describe('with value', function () {
+    it('should return a promise resolved with given value', function (done) {
+      var obj = {};
+      Deferred.resolve(obj)
+        .then(function (value) {
+          expect(value).toBe(obj);
+          done();
+        });
+    });
   });
 
-  it('with value', function (done) {
-    var obj = {};
-    Deferred.resolve(obj)
-      .then(function (value) {
-        expect(value).toBe(obj);
-        done();
-      });
+  describe('with no argument', function () {
+    it('should return a promise resolved without value', function (done) {
+      Deferred.resolve()
+        .then(function (value) {
+          expect(value).toBe(void 0);
+          done();
+        });
+    });
   });
 
-  it('with no argument', function (done) {
-    Deferred.resolve()
-      .then(function (value) {
-        expect(value).toBe(void 0);
-        done();
-      });
+  describe('with undefined', function () {
+    it('should return a promise resolved with undefined', function () {
+      Deferred.resolve(undefined)
+        .then(function (value) {
+          expect(value).toBe(void 0);
+          done();
+        });
+    });
   });
 
-  it('with undefined', function () {
-    Deferred.resolve(undefined)
-      .then(function (value) {
-        expect(value).toBe(void 0);
-        done();
-      });
+  describe('with a promise', function () {
+    it('should return given promise itself', function () {
+      var dfd = new Deferred();
+      expect(Deferred.resolve(dfd.promise)).toBe(dfd.promise);
+    });
   });
 
-  it('with promise', function () {
-    var dfd = new Deferred();
-    expect(Deferred.resolve(dfd.promise)).toBe(dfd.promise);
-  });
-
-  it('with deferred', function () {
-    var dfd = new Deferred();
-    expect(Deferred.resolve(dfd)).toBe(dfd.promise);
+  describe('with deferred', function () {
+    it('should return a promise of given deferred', function () {
+      var dfd = new Deferred();
+      expect(Deferred.resolve(dfd)).toBe(dfd.promise);
+    });
   });
 
   describe('with thenable', function () {
-    it('resolve', function (done) {
+    it('should return a pending promise that will be resolved once given thenable object is resolved', function (done) {
       var value = {};
 
       var thenable = {
@@ -64,7 +70,7 @@ describe('Deferred.resolve', function () {
         });
     });
 
-    it('reject', function (done) {
+    it('should return a pending promise that will be rejected once given thenable object is rejected', function (done) {
       var reason = {};
       var thenable = {
         then: function (onResolve, onReject) {
@@ -82,31 +88,6 @@ describe('Deferred.resolve', function () {
       promise
         .then(null, function () {
           expect(arguments[0]).toBe(reason);
-          done();
-        });
-    });
-
-    it('exception before resolve', function (done) {
-      var value = {};
-      var err = new Error();
-
-      var thenable = {
-        then: function (onResolve) {
-          setTimeout(function () {
-            throw err;
-            onResolve(value);
-          }, 100);
-        }
-      };
-
-      var promise = Deferred.resolve(thenable);
-
-      expect(Deferred.isPromise(promise)).toBe(true);
-      expect(promise.isPending()).toBe(true);
-
-      promise
-        .then(null, function (reason) {
-          expect(reason).toBe(err);
           done();
         });
     });
